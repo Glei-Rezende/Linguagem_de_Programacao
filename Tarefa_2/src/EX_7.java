e23720dbc7d8c8309dd9360fb9403a281a57a351
Gleialison <gleialison.rezende@fatec.sp.gov.br>
2024-04-15 21:00:34 -0300

    Organizando o GitHub
    Organizando as atividades no GitHub

---
+++ Tarefa_2/src/EX_7.java
@@ -0,0 +1,23 @@
+package linguagem_programacao;
+
+
+import java.util.Scanner;
+
+public class EX_7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Leitura do valor
+        System.out.print("Digite um valor: ");
+        int valor = scanner.nextInt();
+
+        // Verificação e exibição da mensagem
+        if (valor > 10) {
+            System.out.println("É MAIOR QUE 10!");
+        } else {
+            System.out.println("NÃO É MAIOR QUE 10!");
+        }
+
+        scanner.close();
+    }
+}
